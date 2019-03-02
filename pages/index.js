f216cefd4544fcc67d88ed423b570b5a4f66b00c
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-03-02 13:51:15 +0100

    Initial commit; project setup

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+const Home = () => (
+  <>
+    <h1>Hacker Next</h1>
+    <style global jsx>{`
+      body {
+        background: #333;
+        color: lightgrey;
+      }
+    `}</style>
+  </>
+)
+
+export default Home
