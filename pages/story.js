29bb628e729947f02f1e3c4bc99ad46d8a8bb9f1
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-03-03 10:34:28 +0100

    Add the page for display a single story with nested comments

---
+++ pages/story.js
@@ -0,0 +1,44 @@
+import Error from 'next/error'
+import fetch from 'isomorphic-unfetch'
+
+import Layout from '../components/Layout'
+import Footer from '../components/Footer'
+import Story from '../components/Story'
+import CommentList from '../components/CommentList'
+
+const StoryPage = ({ story }) => (
+  <Layout title={story && story.title}>
+    {!story ? (
+      <Error statusCode={503} />
+    ) : (
+      <>
+        <main>
+          <Story story={story} />
+          <CommentList comments={story.comments} />
+        </main>
+        <Footer />
+      </>
+    )}
+    <style jsx>{`
+      main {
+        padding: 0 1rem;
+      }
+    `}</style>
+  </Layout>
+)
+
+StoryPage.getInitialProps = async ({ req, res, query }) => {
+  const storyId = query.id
+  let story
+  try {
+    const res = await fetch(`https://node-hnapi.herokuapp.com/item/${storyId}`)
+    story = await res.json()
+  } catch (err) {
+    console.error('StoryPage getInitialProps', err)
+    story = null
+  }
+
+  return { story }
+}
+
+export default StoryPage
