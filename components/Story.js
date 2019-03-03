5d7a9860ad9914bef600b7705f7c47b8055baa36
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-03-03 10:25:27 +0100

    Update Story component for re-use it.

--- components/Story.js
+++ components/Story.js
@@ -1,28 +1,38 @@
 import Link from 'next/link'
 
-const Story = ({ id, title, url, points, commentsCount, timeAgo, user }) => (
-  <section className='story' key={id}>
-    <h2 className='story-title'>
-      <a href={url} title={title} target='_blank'>
-        {title}
-      </a>
-    </h2>
-    <article className='story-details'>
-      <span>
-        Posted {timeAgo} by {user}
-      </span>
-      <span>
-        {points || 0} point{points > 1 ? 's' : ''}
-      </span>
-      <Link href={`/story?id=${id}`}>
-        <a>
-          {commentsCount} comment
-          {commentsCount > 1 ? 's' : ''}
+const Story = ({ story }) => (
+  <>
+    <section className='story' key={story.id}>
+      <h2 className='story-title'>
+        <a href={story.url} title={story.title} target='_blank'>
+          {story.title}
         </a>
-      </Link>
-    </article>
+      </h2>
+      <article className='story-details'>
+        <span>
+          Posted {story.time_ago} by {story.user}
+        </span>
+        <span>
+          {story.points || 0} point{story.points > 1 ? 's' : ''}
+        </span>
+        {story.comments ? (
+          <span>
+            {story.comments_count} comment
+            {story.comments_count > 1 ? 's' : ''}
+          </span>
+        ) : (
+          <Link href={`/story?id=${story.id}`}>
+            <a>
+              {story.comments_count} comment
+              {story.comments_count > 1 ? 's' : ''}
+            </a>
+          </Link>
+        )}
+      </article>
+    </section>
     <style jsx>{`
       .story {
+        border-bottom: 1px solid rgba(255, 255, 255, 0.1);
         padding: 1rem 0;
       }
       .story-title {
@@ -43,6 +53,7 @@ const Story = ({ id, title, url, points, commentsCount, timeAgo, user }) => (
         color: #aaa;
         font-size: 0.8rem;
         font-weight: bold;
+        margin-left: 0.5rem;
       }
       .story-details span {
         margin-right: 1rem;
@@ -52,7 +63,7 @@ const Story = ({ id, title, url, points, commentsCount, timeAgo, user }) => (
         text-decoration: none;
       }
     `}</style>
-  </section>
+  </>
 )
 
 export default Story
