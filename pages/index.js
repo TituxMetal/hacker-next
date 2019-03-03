636465b15996d59ab8cfb9b3aeb5bb6ac15d0640
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-03-03 04:57:37 +0100

    Update index page; display stories from node hacker news api

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
-const Home = () => (
-  <>
-    <h1>Hacker Next</h1>
-    <style global jsx>{`
-      body {
-        background: #333;
-        color: lightgrey;
-      }
-    `}</style>
-  </>
+import Error from 'next/error'
+import fetch from 'isomorphic-unfetch'
+
+import Layout from '../components/Layout'
+import StoryList from '../components/StoryList'
+
+const Home = ({ stories }) => (
+  <Layout
+    title='Hacker Next'
+    description='A Hacker News clone made with Next.js'
+  >
+    {stories.length === 0 ? (
+      <Error statusCode={503} />
+    ) : (
+      <div>
+        <StoryList stories={stories} />
+      </div>
+    )}
+  </Layout>
 )
+
+Home.getInitialProps = async () => {
+  let stories
+  try {
+    const res = await fetch(`https://node-hnapi.herokuapp.com/news?page=1`)
+    stories = await res.json()
+  } catch (err) {
+    console.error('Home getInitialProps', err)
+    stories = []
+  }
+
+  return { stories }
+}
 
 export default Home
